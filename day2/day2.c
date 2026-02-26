86eaad3644799c89cc63ec3fe1eb429dd59813ca
Miguel Loranca <mloranca703@gmail.com>
2026-02-25 18:42:29 -0600

    added the reverse in place function using a double pointer approach, this is better than iterating 3 times like the other function was doing before, is the in place function we only iterate once and we dont create any new arrays

--- day2/day2.c
+++ day2/day2.c
@@ -1,3 +1,7 @@
+/*
+ * reverse a string in place
+ */
+
 #include <stdio.h>
 #include <stddef.h>
 
@@ -8,7 +12,8 @@ void char_to_lower(char *c);
 
 void str_to_capital(char *str);
 void str_to_lower(char *str);
-void str_reverse(char *str);
+int str_reverse(char *str);
+int str_reverse_in_place(char *str);
 
 int main(void) {
     char str[] = "abcdefg";
@@ -18,13 +23,25 @@ int main(void) {
     str_to_lower(str);
     puts(str);
 
-    str_reverse(str);
+    if (str_reverse(str) < 0) {
+        fprintf(stderr, "Error while reversing string\n");
+        return -1;
+    }
+    puts(str);
+
+    if (str_reverse_in_place(str) < 0) {
+        fprintf(stderr, "Error while reversing string\n");
+        return -1;
+    }
     puts(str);
 
     return 0;
 }
 
 size_t str_len(char *str) {
+    if (!str)
+        return 0;
+
     size_t len = 0;
     while (str[++len])
         ;;
@@ -32,27 +49,46 @@ size_t str_len(char *str) {
 }
 
 void char_to_capital(char *c) {
+    if (!c)
+        return;
+
     *c += ('A' - 'a');
 }
 
 void char_to_lower(char *c) {
+    if (!c)
+        return;
+
     *c -= ('A' - 'a');
 }
 
 void str_to_capital(char *str) {
+    if (!str)
+        return;
+
     for (size_t i = 0; str[i] != '\0'; ++i) {
         char_to_capital(&str[i]);
     }
 }
 
 void str_to_lower(char *str) {
+    if (!str)
+        return;
+
     for (size_t i = 0; str[i] != '\0'; ++i) {
         char_to_lower(&str[i]);
     }
 }
 
-void str_reverse(char *str) {
+int str_reverse(char *str) {
+    if (!str)
+        return -1;
+
     size_t len = str_len(str);
+
+    if (len == 0)
+        return -1;
+
     char reversed[len];
     for (int i = (int)len - 1; i >= 0; --i) {
         reversed[len - (size_t)i - 1] = str[i];
@@ -61,4 +97,29 @@ void str_reverse(char *str) {
     for (size_t i = 0; i < len; ++i) {
         str[i] = reversed[i];
     }
+
+    return 0;
+}
+
+int str_reverse_in_place(char *str) {
+    if (!str)
+        return -1;
+
+    size_t len = str_len(str);
+
+    if (len == 0)
+        return -1;
+
+    int left = 0;
+    int right = (int)len - 1;
+
+    while (left < right) {
+        char tmp = str[left];
+        str[left] = str[right];
+        str[right] = tmp;
+        left++;
+        right--;
+    }
+
+    return 0;
 }
