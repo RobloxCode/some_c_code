785ebb62893a8c780f96383bf537b934cca2b888
Miguel Loranca <mloranca703@gmail.com>
2026-03-03 22:33:59 -0600

    made the pascal triangle function using a formula

--- day5/day5.c
+++ day5/day5.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../utils/ArrayList/ArrayListImp.h"
 
-void print_pascal_triangle(int rows);
+void print_pascals_triangle(int n);
 
 int main(void) {
-    ArrayList *al = ArrayList_create(5);
-    if (!al)
-        return EXIT_FAILURE;
-
+    print_pascals_triangle(7);
     return EXIT_SUCCESS;
 }
 
-void print_pascal_triangle(int rows) {
-    if (rows <= 0) {
+void print_pascals_triangle(int n)
+{
+    if (n <= 0) {
+        printf("N must be greater than 0\n");
         return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int value = 1;
+
+        for (int j = 0; j <= i; j++) {
+            printf("%4d", value);
+            value = value * (i - j) / (j + 1);
+        }
+
+        printf("\n");
     }
 }
