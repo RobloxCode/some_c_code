b082ba63dc6bebfa051e9eac19e0ab29f7f4fae1
Miguel Loranca <mloranca703@gmail.com>
2026-03-05 19:31:09 -0600

    in the day6 file instead of having all the String code there i imported it from the utils file, and added some tests with the String data structure

--- day6/day6.c
+++ day6/day6.c
@@ -3,29 +3,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <limits.h>
-
-typedef enum {
-    STRING_OK,
-    STRING_ERR_WRONG_PTR,
-    STRING_ERR_REALLOC,
-    STRING_ERR_OVERFLOW,
-} String_status;
-
-typedef struct {
-    char *items;
-    size_t length;
-    size_t capacity;
-} String;
-
-String *String_create(size_t init_len);
-String_status String_append(String *str, const char item);
-String_status String_free(String **str);
-String_status String_print(const String *str);
-String_status String_write_to(
-    String *str,
-    const char *content,
-    const size_t content_len
-);
+#include "../utils/String/StringImp.h"
 
 int ascii_to_int(const char *str, int *out);
 size_t get_str_len(char *str);
@@ -49,84 +27,6 @@ int main(int argc, char **argv) {
     }
 
     return EXIT_SUCCESS;
-}
-
-String *String_create(size_t init_len) {
-    if (init_len == 0) {
-        init_len = 1;
-    }
-
-    char *items = calloc(init_len, sizeof *items);
-    if (!items) {
-        return NULL;
-    }
-
-    String *str = malloc(sizeof *str);
-    if (!str) {
-        free(items);
-        return NULL;
-    }
-
-    str->items = items;
-    str->capacity = init_len;
-    str->length = 0;
-
-    return str;
-}
-
-String_status String_append(String *str, const char item) {
-    if (!str) {
-        return STRING_ERR_WRONG_PTR;
-    }
-
-    if (str->length == str->capacity) {
-        if (str->capacity > SIZE_MAX / 2) {
-            return STRING_ERR_OVERFLOW;
-        }
-
-        size_t new_len = str->capacity * 2;
-
-        if (new_len > SIZE_MAX / sizeof *str->items) {
-            return STRING_ERR_OVERFLOW;
-        }
-
-        char *tmp = realloc(str->items, new_len * sizeof *str->items);
-        if (!tmp) {
-            return STRING_ERR_REALLOC;
-        }
-
-        str->items = tmp;
-        str->capacity = new_len;
-    }
-
-    str->items[str->length] = item;
-    str->length++;
-
-    return STRING_OK;
-}
-
-String_status String_free(String **str) {
-    if (!str || !*str) {
-        return STRING_ERR_WRONG_PTR;
-    }
-
-    free((*str)->items);
-    free(*str);
-    *str = NULL;
-    return STRING_OK;
-}
-
-String_status String_print(const String *str) {
-    if (!str) {
-        return STRING_ERR_WRONG_PTR;
-    }
-
-    for (size_t i = 0; i < str->length; ++i) {
-        printf("%c", str->items[i]);
-    }
-
-    printf("\n");
-    return STRING_OK;
 }
 
 int ascii_to_int(const char *str, int *out) {
@@ -213,6 +113,10 @@ int evaluate_str(char *str) {
             printf("%d * %d = %d\n", num1, num2, num1 * num2);
             break;
         case '/':
+            if (num2 == 0) {
+                fprintf(stderr, "cannot divide by 0\n");
+                break;
+            }
             printf("%d / %d = %d\n", num1, num2, num1 / num2);
             break;
         default:
--- test/test.c
+++ test/test.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../utils/String/StringImp.h"
 
 int main(void) {
-    String *s = String_create(10);
-    if (!s)
-        return 1;
+    const size_t string_init_len = 10;
+    int comparisson = 0;
+    String_status status = STRING_OK;
+    String *s1 = String_create(string_init_len);
+    if (!s1) {
+        goto cleanup;
+    }
 
-    String_write_to(s, "i wanna write to this msg");
+    String *s2 = String_create(string_init_len);
+    if (!s2) {
+        goto cleanup;
+    }
 
-    String_print(s);
-    String_free(&s);
-    return 0;
+    if ((status = String_write_to(s1, "this is the string1")) != STRING_OK) {
+        goto cleanup;
+    }
+
+    if ((status = String_write_to(s2, "this is the string1")) != STRING_OK) {
+        goto cleanup;
+    }
+
+    if ((status = String_compare(s1, s2, &comparisson)) != STRING_OK) {
+        goto cleanup;
+    }
+
+    if (comparisson) {
+        printf("the strings\n");
+        String_print(s1);
+        String_print(s2);
+        printf("are equal\n");
+    } else {
+        printf("the strings\n");
+        String_print(s1);
+        String_print(s2);
+        printf("are different\n");
+    }
+
+    cleanup:
+        if (s1) {
+            String_free(&s1);
+        }
+
+        if (s2) {
+            String_free(&s2);
+        }
+
+        if (status != STRING_OK) {
+            fprintf(stderr, "Error status: %d\n", status);
+            return EXIT_FAILURE;
+        }
+
+    return EXIT_SUCCESS;
 }
