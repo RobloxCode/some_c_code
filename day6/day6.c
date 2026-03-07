99c49aeca476a5294cdee7271f99925002abc037
Miguel Loranca <mloranca703@gmail.com>
2026-03-07 17:01:26 -0600

    added an overflow check in the ascii_to_int function, also fixed a case where the user could input a number with a + sign at the start, added a clear_str function which takes a char * and sets every item to '\0', addde the is_operator, is_digit, is_space functions, and made that the function evaluate_exp makes an array of numbers and in the parse function almost everything is working, i just have an off by one error, gotta check that, i basically have a buffer which will contain each number from the expression just so that i can cast it to an int, once i find an operator i will cast whatever the buffer has at that point, but it always misses the last number

--- day6/day6.c
+++ day6/day6.c
@@ -3,52 +3,80 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <limits.h>
-#include "../utils/String/StringImp.h"
+#define MAX_DIGIT_LEN 9
+#define MAX_DIGITS_IN_EXPRESSION 10
 
-int ascii_to_int(const char *str, int *out);
+typedef struct {
+    int *items;
+    size_t len;
+} IntArray;
+
+int ascii_to_int(const char *str, int *result);
 size_t get_str_len(char *str);
-String *parse_str(char *str);
-int evaluate_str(char *str);
+int clear_str(char *str);
+int is_digit(char c);
+int is_operator(char c);
+int is_space(char c);
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr,
-            "USAGE:\n"
-            "./d6 [some expression]\n"
-            "ex:\n"
-            "./d6 \"1 + 1\"\n"
-        );
-        return EXIT_FAILURE;
-    }
+int parse_exp(char *exp, IntArray *digits);
+int evaluate_exp(char *exp, int *result);
 
-    char *usr_str = argv[1];
-    if (evaluate_str(usr_str) < 0) {
+int main(void) {
+    // if (argc != 2) {
+    //     fprintf(stderr,
+    //         "USAGE:\n"
+    //         "./d6 [some expression]\n"
+    //         "ex:\n"
+    //         "./d6 \"1 + 1\"\n"
+    //     );
+    //     return EXIT_FAILURE;
+    // }
+
+    char *expression = "132 + 43 - 54";
+    int result = 0;
+    if (evaluate_exp(expression, &result) < 0) {
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
 }
 
-int ascii_to_int(const char *str, int *out) {
+int ascii_to_int(const char *str, int *result) {
     if (!str) {
         return -1;
     }
 
     size_t i = 0;
     int sign = 1;
-    int result = 0;
 
     if (str[i] == '-') {
         i++;
         sign = -1;
+    } else if (str[i] == '+') {
+        i++;
     }
 
     for (; str[i] != '\0'; ++i) {
+        if (!is_digit(str[i])) {
+            return -1;
+        }
+
         int digit = str[i] - '0';
-        result = result * 10 + digit;
+
+        if (sign == 1) {
+            if (*result > (INT_MAX - digit) / 10) {
+                return -1;
+            }
+        } else {
+            if (*result > (-(INT_MIN + digit)) / 10) {
+                return -1;
+            }
+        }
+
+        *result = *result * 10 + digit;
     }
 
-    *out = result * sign;
+    *result = *result * sign;
     return 0;
 }
 
@@ -64,65 +92,81 @@ size_t get_str_len(char *str) {
     return i;
 }
 
-String *parse_str(char *str) {
+int clear_str(char *str) {
     if (!str) {
-        return NULL;
+        return -1;
     }
 
-    size_t str_len = get_str_len(str);
-    String_status status = STRING_OK;
-    String *dynamic_str = String_create(str_len);
-    if (!dynamic_str) {
-        return NULL;
+    for (size_t i = 0; str[i] != '\0'; ++i) {
+        str[i] = '\0';
     }
 
-    for (size_t i = 0; i < str_len; ++i) {
-        if (str[i] != ' ') {
-            if ((status = String_append(dynamic_str, str[i])) != STRING_OK) {
-                fprintf(stderr, "Error status: %d\n", status);
-                return NULL;
+    return 0;
+}
+
+int is_digit(char c) {
+    if (c < '0' || c > '9') {
+        return 0;
+    }
+    return 1;
+}
+
+int is_operator(char c) {
+    if (c == '+' || c == '-' || c == '/' || c == '*') {
+        return 1;
+    }
+    return 0;
+}
+
+int is_space(char c) {
+    return c == ' ';
+}
+
+int parse_exp(char *exp, IntArray *digits) {
+    if (!exp) {
+        return -1;
+    }
+
+    int atoi_res = 0;
+    char ascii_cur_digit[MAX_DIGIT_LEN] = {'\0'};
+    size_t char_append_pos = 0;
+    size_t digit_append_pos = 0;
+
+    for (size_t i = 0; i < get_str_len(exp); ++i) {
+        if (is_operator(exp[i]) || exp[i + 1] == '\0') {
+            if ((ascii_to_int(ascii_cur_digit, &atoi_res)) < 0) {
+                return -1;
             }
+
+            digits->items[digit_append_pos] = atoi_res;
+            digit_append_pos++;
+            atoi_res = 0;
+            char_append_pos = 0;
+            clear_str(ascii_cur_digit);
+            continue;
+        } else if (is_space(exp[i])) {
+            continue;
+        } if (is_digit(exp[i])) {
+            ascii_cur_digit[char_append_pos] = exp[i];
+            char_append_pos++;
         }
     }
 
-    return dynamic_str;
+    return 0;
 }
 
-int evaluate_str(char *str) {
-    if (!str) {
+int evaluate_exp(char *exp, int *result) {
+    if (!exp || !result) {
         return -1;
     }
 
-    String *dynamic_str = parse_str(str);
-    if (!dynamic_str) {
-        return -1;
+    int items[MAX_DIGITS_IN_EXPRESSION] = {0};
+    IntArray digits = (IntArray){ .items = items, .len = (size_t)MAX_DIGITS_IN_EXPRESSION };
+    parse_exp(exp, &digits);
+
+    for (size_t i = 0; i < digits.len; ++i) {
+        printf("%d ", digits.items[i]);
     }
 
-    int num1 = dynamic_str->items[0] - '0';
-    int num2 = dynamic_str->items[2] - '0';
-    char sign = dynamic_str->items[1];
-
-    switch (sign) {
-        case '+':
-            printf("%d + %d = %d\n", num1, num2, num1 + num2);
-            break;
-        case '-':
-            printf("%d - %d = %d\n", num1, num2, num1 - num2);
-            break;
-        case '*':
-            printf("%d * %d = %d\n", num1, num2, num1 * num2);
-            break;
-        case '/':
-            if (num2 == 0) {
-                fprintf(stderr, "cannot divide by 0\n");
-                break;
-            }
-            printf("%d / %d = %d\n", num1, num2, num1 / num2);
-            break;
-        default:
-            printf("not a valid operator\n");
-    }
-
-    String_free(&dynamic_str);
     return 0;
 }
