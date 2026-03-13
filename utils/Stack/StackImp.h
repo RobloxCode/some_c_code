f82444f2fdf1165784b571a2681e813377f7abf0
Miguel Loranca <mloranca703@gmail.com>
2026-03-13 17:18:08 -0600

    create the Stack file in the utils file and in the day8 imported the stack instead of having everything there

---
+++ utils/Stack/StackImp.h
@@ -0,0 +1,24 @@
+#ifndef STACKIMP_H
+#define STACKIMP_H
+#include <stddef.h>
+typedef struct {
+    int *items;
+    size_t len;
+    size_t cap;
+} Stack;
+
+typedef enum {
+    STACK_OK,
+    STACK_ERR_WRONG_PTR,
+    STACK_ERR_REALLOC,
+    STACK_ERR_EMTPY,
+} Stack_status;
+
+Stack *Stack_init(const size_t init_cap);
+Stack_status Stack_free(Stack **stack);
+Stack_status Stack_push(Stack *stack, const int item);
+Stack_status Stack_pop(Stack *stack, int *out);
+Stack_status Stack_top(Stack *stack, int *out);
+Stack_status Stack_println(const Stack *stack);
+
+#endif
