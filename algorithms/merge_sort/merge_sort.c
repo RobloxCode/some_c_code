e772d9f15064b0fef1daa895a63e06a41df5011d
Miguel Loranca <mloranca703@gmail.com>
2026-04-05 17:40:01 -0600

    made the merge sort algorithm

--- algorithms/merge_sort/merge_sort.c
+++ algorithms/merge_sort/merge_sort.c
@@ -1,52 +1,82 @@
 #include <stdio.h>
 #include <stddef.h>
 
-void merge(int *arr, int *temp, int left, int mid, int right) {
-    int i = left;     // left subarray
-    int j = mid + 1;  // right subarray
-    int k = left;     // temp index
-
-    while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j])
-            temp[k++] = arr[i++];
-        else
-            temp[k++] = arr[j++];
-    }
-
-    while (i <= mid)
-        temp[k++] = arr[i++];
-
-    while (j <= right)
-        temp[k++] = arr[j++];
-
-    // copy back to original array
-    for (int x = left; x <= right; x++)
-        arr[x] = temp[x];
-}
-
-void merge_sort(int *arr, int *temp, int left, int right) {
-    if (left >= right)
-        return;
-
-    int mid = (left + right) / 2;
-
-    merge_sort(arr, temp, left, mid);
-    merge_sort(arr, temp, mid + 1, right);
-    merge(arr, temp, left, mid, right);
-}
+void _arr_sort_helper(
+    int *arr,
+    int *tmp,
+    size_t left,
+    size_t mid,
+    size_t right
+);
+void arr_sort(
+    int *arr,
+    int *tmp,
+    size_t left,
+    size_t right
+);
+void arr_println(const int *arr, const size_t len);
 
 int main() {
-    int arr[] = {5, 2, 9, 1, 3};
-    int n = sizeof arr / sizeof arr[0];
+    int arr[] = {5, 2, 9, 1, 3, 3, 5, 0, 4, 6, 10, 7};
+    size_t len = sizeof arr / sizeof arr[0];
 
-    int temp[n];  // stack-allocated (no malloc)
+    int temp[len];
 
-    merge_sort(arr, temp, 0, n - 1);
+    arr_sort(arr, temp, 0, (size_t)len - 1);
 
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    arr_println(arr, len);
 
     printf("\n");
 
     return 0;
+}
+
+void _arr_sort_helper(
+    int *arr,
+    int *tmp,
+    size_t left,
+    size_t mid,
+    size_t right
+) {
+    size_t left_idx = left;
+    size_t right_idx = mid + 1;
+    size_t tmp_idx = left;
+
+    while (left_idx <= mid && right_idx <= right) {
+        if (arr[left_idx] <= arr[right_idx])
+            tmp[tmp_idx++] = arr[left_idx++];
+        else
+            tmp[tmp_idx++] = arr[right_idx++];
+    }
+
+    while (left_idx <= mid)
+        tmp[tmp_idx++] = arr[left_idx++];
+
+    while (right_idx <= right)
+        tmp[tmp_idx++] = arr[right_idx++];
+
+    for (int x = (int)left; x <= (int)right; ++x)
+        arr[x] = tmp[x];
+}
+
+void arr_sort(
+    int *arr,
+    int *tmp,
+    size_t left,
+    size_t right
+) {
+    if (left >= right)
+        return;
+
+    size_t mid = (left + right) / 2;
+
+    arr_sort(arr, tmp, left, mid);
+    arr_sort(arr, tmp, mid + 1, right);
+    _arr_sort_helper(arr, tmp, left, mid, right);
+}
+
+void arr_println(const int *arr, const size_t len) {
+    for (size_t i = 0; i < len; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
