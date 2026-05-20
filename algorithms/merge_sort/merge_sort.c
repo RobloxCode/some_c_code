bd673c26b54ac319db8fc9119e76b743bc1f51fd
Miguel Loranca <mloranca703@gmail.com>
2026-05-19 21:56:43 -0600

    updated the format code in the merge sort algorithm

--- algorithms/merge_sort/merge_sort.c
+++ algorithms/merge_sort/merge_sort.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stddef.h>
 
-void _arr_sort_helper(
-    int *arr,
-    int *tmp,
-    const size_t left,
-    const size_t mid,
-    const size_t right
-);
-void arr_sort(
-    int *arr,
-    int *tmp,
-    const size_t left,
-    const size_t right
-);
+void _arr_sort_helper(int *arr,
+                      int *tmp,
+                      const size_t left,
+                      const size_t mid,
+                      const size_t right);
+void arr_sort(int *arr,
+              int *tmp,
+              const size_t left,
+              const size_t right);
 void arr_println(const int *arr, const size_t len);
 
-int main() {
+int main(void)
+{
     int arr[] = {5, 2, 9, 1, 3, 3, 5, 0, 4, 6, 10, 7};
     size_t len = sizeof arr / sizeof arr[0];
-
     int temp[len];
 
     arr_sort(arr, temp, 0, (size_t)len - 1);
-
     arr_println(arr, len);
 
     printf("\n");
@@ -31,13 +26,12 @@ int main() {
     return 0;
 }
 
-void _arr_sort_helper(
-    int *arr,
-    int *tmp,
-    const size_t left,
-    const size_t mid,
-    const size_t right
-) {
+void _arr_sort_helper(int *arr,
+                      int *tmp,
+                      const size_t left,
+                      const size_t mid,
+                      const size_t right)
+{
     size_t left_idx = left;
     size_t right_idx = mid + 1;
     size_t tmp_idx = left;
@@ -59,12 +53,11 @@ void _arr_sort_helper(
         arr[x] = tmp[x];
 }
 
-void arr_sort(
-    int *arr,
-    int *tmp,
-    const size_t left,
-    const size_t right
-) {
+void arr_sort(int *arr,
+              int *tmp,
+              const size_t left,
+              const size_t right)
+{
     if (left >= right)
         return;
 
@@ -75,7 +68,8 @@ void arr_sort(
     _arr_sort_helper(arr, tmp, left, mid, right);
 }
 
-void arr_println(const int *arr, const size_t len) {
+void arr_println(const int *arr, const size_t len)
+{
     for (size_t i = 0; i < len; ++i)
         printf("%d ", arr[i]);
     printf("\n");
